53acbef0fbd14c1418aac6aad9bb11714b242c8e
“Zoe <z.oellinger@students.htl-leonding.ac.at>
2024-09-27 13:15:12 +0200

    mau

--- quarks-primar/src/main/java/at/htlleonding/GreetingResource.java
+++ quarks-primar/src/main/java/at/htlleonding/GreetingResource.java
@@ -7,7 +7,7 @@ import jakarta.ws.rs.core.MediaType;
 
 @Path("/hello")
 public class GreetingResource {
-
+    private int maumau = 3;
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
